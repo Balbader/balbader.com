59c56717fd9e82bcd12a7b2d6eff6ce1e499c733
BASIL ALBADER <basilalbapro@gmail.com>
2025-06-30 15:51:20 +0200

    WIP

---
+++ pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<div className="flex flex-col gap-[32px] items-center sm:items-start">
+			<h1 className="text-4xl font-bold">Home</h1>
+		</div>
+	);
+}
